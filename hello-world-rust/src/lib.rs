e91906e57ffd7afc48c46942dec477cbb1067578
George Thomas <gerogethomas.mec@gmail.com>
2024-06-20 22:54:47 -0700

    change message

--- hello-world-rust/src/lib.rs
+++ hello-world-rust/src/lib.rs
@@ -2,5 +2,9 @@ use worker::*;
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    Response::ok("Hello. Is the fish allive?")
+    if !matches!(req.method(), Method::Get) {
+        return Response::error("Please use Post to send an image", 405);
+    }
+
+    return Response::ok("Got a non get request");
 }
